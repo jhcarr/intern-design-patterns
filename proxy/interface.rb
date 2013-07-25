4ca1bbc172dc9b098f01c0197ab1ee8ccba33bc9
Will Hopper <whopper@cat.pdx.edu>
2013-07-24 17:28:09 -0700

    Add proxy directory with sample proxy pattern code

---
+++ proxy/interface.rb
@@ -0,0 +1,20 @@
+# A fake GUI that this app may run in real life
+
+class Interface
+
+  def initialize
+    puts 'A graphical interface appears before you...'
+  end
+
+  def openMainMenu
+    puts 'The main menu opens...'
+  end
+
+  def openOptions
+    puts 'The options menu opens...'
+  end
+
+  def closeMenus
+    puts 'The menus vanish before your eyes...'
+  end
+end
